4d3a15108123ab63e2ff24a140308ed8e94547c7
Keira Hartley <khartley@lcs.on.ca>
2020-01-22 12:00:41 -0500

    I got the background and bad code.

--- Gig Poster 3.playground/Pages/Velvet Underground.xcplaygroundpage/Contents.swift
+++ Gig Poster 3.playground/Pages/Velvet Underground.xcplaygroundpage/Contents.swift
@@ -31,13 +31,54 @@ let purple = Color(hue: 309, saturation: 78, brightness: 64, alpha: 100)
 let offWhite = Color(hue: 83, saturation: 4, brightness: 89, alpha: 100)
 
 // Begin your solution here...
+canvas.fillColor = black
+canvas.drawRectangle(at: Point(x:0, y:0), width: 400, height: 600)
+
+canvas.fillColor = purple
+
+var xCoordinateStart = 365
+var yCoordinateStart = 10
+
+var xCoordinateEnd = 400
+var yCoordinateEnd = 65
+
+
+//canvas.lineColor = purple
+//canvas.drawLine(from: Point(x:10, y:0), to: Point(x:10, y:65), lineWidth: 20)
+//canvas.drawLine(from: Point(x:0, y:10), to: Point(x:65, y:10), lineWidth: 20)
+//canvas.drawLine(from: Point(x:10, y:10), to: Point(x:75, y:75), lineWidth: 20)
+
+canvas.fillColor = purple
+
+
+
+for _ in 1...5{
+    
+    
+    for x in stride(from: 0, to:
+        400, by: 80){
+            canvas.lineColor = purple
+            canvas.drawLine(from: Point(x:x, y:10), to: Point(x: xCoordinateEnd, y: 10), lineWidth: 20)
+            
+            
+    }
+    yCoordinateStart += 80
+    xCoordinateStart -= 80
+    
+    yCoordinateEnd += 80
+    xCoordinateEnd -= 80
+    
+}
+
+
+
 
 
 /*:
  ## Use Source Control
  
  Remember to commit and push your work before 12:05 PM on Wednesday, January 22, 2020, please.
-
+ 
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
